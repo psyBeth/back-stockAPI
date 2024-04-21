edc0c21de7a53028abe0bf852a7307c0fd5e6413
Betul <betulkoru06@gmail.com>
2024-04-21 18:24:22 +0300

    warnings solved dbconnect

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 
 /* ------------------------------------------------------- */
 // Required Modules:
@@ -16,34 +17,36 @@ const PORT = process.env?.PORT || 8000
 // asyncErrors to errorHandler:
 require('express-async-errors')
 
+app.use(express.static(path.join(__dirname, "public")));
+
 /* ------------------------------------------------------- */
 // Configrations:
 
 // Connect to DB:
 const { dbConnection } = require('./src/configs/dbConnection')
-dbConnection()
+dbConnection();
 
 /* ------------------------------------------------------- */
 // Middlewares:
 
 // Accept JSON:
-app.use(express.json())
+app.use(express.json());
 
 // Cors:
-app.use(require('cors')())
+app.use(require('cors')());
 
 // Check Authentication:
-app.use(require('./src/middlewares/authentication'))
+app.use(require('./src/middlewares/authentication'));
 
 // res.getModelList():
-app.use(require('./src/middlewares/findSearchSortPage'))
+app.use(require('./src/middlewares/findSearchSortPage'));
 
 /* ------------------------------------------------------- */
 // Routes:
 
 // HomePath:
 app.all("/api/v1/documents", (req, res) => {
-    
+
     res.send(`
         <h3>Stock Management API Service</h3>
         <hr>
@@ -62,6 +65,13 @@ app.all("/api/v1/documents", (req, res) => {
 app.use("/api/v1", require('./src/routes'))
 
 /* ------------------------------------------------------- */
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
+app.use("*", (req, res) => {
+    res.status(404).json({ msg: "not found" });
+});
 
 // errorHandler:
 app.use(require('./src/middlewares/errorHandler'))
--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = function () {
     // Connect:
-    mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGODB)
         .then(() => {
             console.log('* DB Connected * ')
             // Syncronization (must be in commentLine):
